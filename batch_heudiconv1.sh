bf2e71c0adf42c2ce0d5635edfc33c2d2888f9df
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2023-01-09 10:18:38 +0900

    update batch

--- batch_heudiconv1.sh
+++ batch_heudiconv1.sh
@@ -2,23 +2,35 @@
 
 # script to execute heudiconv
 # Usage: batch_heudiconv1 heuristic.py subjlist
-# Please see heuristic_template.py and subjlist_template.txt
+# Please see heuristic_template.py and subjlist.sample.txt
 # to prepare the files
 
 # Prerequisites: install dcm2niix and heudiconv
 
 # Dicom must be sorted beforehand using dcm_sort_dir.py
 
-# K. Nemoto 02 Jan 2023
+# K. Nemoto 08 Jan 2023
 
+set -x
 
-heuristics=$1
+# assign heuristic.py to $heuristic and subjlist.txt to $subjlist
+heuristic=$1
 subjlist=$2
 
-tail +7 ${subjlist} | while read dname subj session
+# Make sure you specify a heuristic.py for the first argument
+heuext=${heuristic##*.}
+if [[ $heuext != 'py' ]]; then
+  echo "Please specify heuristics.py first"
+  echo "Usage: $0 heuristics.py subjlist.txt"
+  exit 1
+fi
+
+# Run heudiconv
+# remove blank line beforehand using sed '/^$/d'
+tail +7 ${subjlist} | sed '/^$/d' | while read dname subj session
 do
   heudiconv -d DICOM/sorted/${dname}/*/*.dcm \
-	-o Nifti -f ${heuristics} \
+	-o Nifti -f ${heuristic} \
 	-s ${subj} -ss ${session} \
 	-c dcm2niix -b --overwrite 
 done
--- batch_heudiconv2.sh
+++ batch_heudiconv2.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-# script to execute heudiconv for double-echo fieldmap
+# script to execute heudiconv for double-echo fieldmap in a special case
 # This will generate a magnitude nifti image
 
 # Usage: batch_heudiconv2 heuristic.py subjlist
-# Please see heuristic_template.py and subjlist_template.txt
+# Please see heuristic_template.py and subjlist.sample.txt
 # to prepare the files
 
 # batch_heudiconv1.sh must be executed befor running this script
 
-# K. Nemoto 02 Jan 2023
+# K. Nemoto 08 Jan 2023
 
 
-heuristics=$1
+set -x
+
+# assign heuristic.py to $heuristic and subjlist.txt to $subjlist
+heuristic=$1
 subjlist=$2
 
-tail +7 subjlist.txt | while read dname subj session
+# Make sure you specify a heuristic.py for the first argument
+heuext=${heuristic##*.}
+if [[ $heuext != 'py' ]]; then
+  echo "Please specify heuristics.py first"
+  echo "Usage: $0 heuristics.py subjlist.txt"
+  exit 1
+fi
+
+# Run heudiconv
+# remove blank line beforehand using sed '/^$/d'
+tail +7 ${subjlist} | sed '/^$/d' | while read dname subj session
 do
   heudiconv -d DICOM/sorted/${dname}/*/*.dcm \
 	-o Nifti -f ${heuristics} \
@@ -23,4 +36,4 @@ do
 	-c dcm2niix -b --overwrite \
         --dcmconfig code/merge.json
 done
-    
+
