87881e2481571db3671807e30e9820a0cbe7e1e1
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2023-02-18 13:37:07 +0900

    prepare files fore cases without sessions

---
+++ batch_heudiconv_wo_session.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# script to execute heudiconv
+# Usage: batch_heudiconv_wo_session.sh heuristic.py subjlist
+# Please see heuristic_template.py and subjlist.sample.txt
+# to prepare the files
+
+# Prerequisites: install dcm2niix and heudiconv
+
+# Dicom must be sorted beforehand using dcm_sort_dir.py
+
+# K. Nemoto 18 Feb 2023
+
+set -x
+
+# assign heuristic.py to $heuristic and subjlist.txt to $subjlist
+heuristic=$1
+subjlist=$2
+
+# Make sure you specify a heuristic.py for the first argument
+heuext=${heuristic##*.}
+if [[ $heuext != 'py' ]]; then
+  echo "Please specify heuristics.py first"
+  echo "Usage: $0 heuristics.py subjlist.txt"
+  exit 1
+fi
+
+# Run heudiconv
+# remove blank line beforehand using sed '/^$/d'
+tail +7 ${subjlist} | sed '/^$/d' | while read dname subj
+do
+  heudiconv -d DICOM/sorted/${dname}/*/* \
+	-o Nifti -f ${heuristic} \
+	-s ${subj}  \
+	-c dcm2niix -b --overwrite 
+done
+
