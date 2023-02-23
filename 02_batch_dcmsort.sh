9e87e479f62a34b43938b87ca153107d612f7b21
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2023-02-23 23:09:42 +0900

    update 02_batch_dcmsort.sh

--- 02_batch_dcmsort.sh
+++ 02_batch_dcmsort.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
+# Script to sort DICOM using dcm_sort_dir.py in batch
+# 23 Feb 2023 K.Nemoto
 
-cd workingDir/DICOM/original
-for dir in *
+# Prerequisite
+# Please place your DICOM directories under DICOM/original
+# sorted directories will be DICOM/sorted
+
+# Acquire parntDir where workingDir exists
+cwd=$(dirname $PWD)
+parentDir=$(echo ${cwd%workingDir*})
+if [[ $? -ne 0 ]]; then
+  echo "You are not in the right directory."
+  echo "Please run this script where you executed 01_prep_heudiconv.sh"
+  exit 1
+fi
+
+# cd DICOM/original
+cd ${parentDir}/workingDir/DICOM/original
+
+# Sort DICOMs
+for dir in $(ls -F | grep /)
 do
   dcm_sort_dir.py $dir
 done
 
-tree workingDir/DICOM/sort -d
+tree ${parentDir}/workingDir/DICOM/sorted -d
 
 exit
 
