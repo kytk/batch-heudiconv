6c14e8a940117d0d85c7b84a44a575937dcc4d92
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2023-05-25 22:55:33 +0900

    add batch_heudiconv_fmap.sh for various field maps

--- batch_heudiconv_fmap.sh
+++ batch_heudiconv_fmap.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# script to execute heudiconv for double-echo fieldmap in a special case
-# This will generate a magnitude nifti image
+# script to execute heudiconv for datasets with various fieldmaps
 
-# Usage: batch_heudiconv_double_echo_in_one_fmap.sh heuristic.py subjlist
+# Usage: batch_heudiconv_fmap.sh heuristic.py subjlist
 # Please see heuristic_template.py and subjlist.sample.txt
 # to prepare the files
 
-# batch_heudiconv.sh must be executed befor running this script
-
-# K. Nemoto 29 Mar 2023
+# 25 May 2023 K. Nemoto
 
 
 set -x
+
+### number of slices of field mapping image ###
+# Adjust with your dataset
+fmap_slices=39
+fmapthr=$((fmap_slices * 2))
+###
+
 
 # assign heuristic.py to $heuristic and subjlist.txt to $subjlist
 heuristic=$1
@@ -26,30 +30,31 @@ if [[ $heuext != 'py' ]]; then
   exit 1
 fi
 
+# delete previous .heudiconv
+[[ -d Nifti/.heudiconv ]] && rm -rf Nifti/.heudiconv 
+
 # Run heudiconv
 # remove blank line beforehand using sed '/^$/d'
 tail +7 ${subjlist} | sed '/^$/d' | while read dname subj session
 do
-  nfiles=$(ls DICOM/sorted/${subj}_${session}/*field_mapping* | wc -w)
-  if [[ $nfiles -le 90 ]]; then
+  ndirs=$(find DICOM/sorted/${subj}_${session} -type d -name '*field_mapping*' | wc -l)
+  nfiles=$(find DICOM/sorted/${subj}_${session} -type f -name '*field_mapping*' | wc -l)
+  if [[ $ndirs -eq 2 ]] && [[ $nfiles -le ${fmapthr} ]]; then
     heudiconv -d DICOM/sorted/${dname}/*/*.dcm \
-	-o Nifti_tmp -f ${heuristic} \
+	-o Nifti -f ${heuristic} \
 	-s ${subj} -ss ${session} \
 	-c dcm2niix -b --overwrite \
 	--dcmconfig code/merge.json
 
   else
     heudiconv -d DICOM/sorted/${dname}/*/*.dcm \
-	-o Nifti_tmp -f ${heuristic} \
+	-o Nifti -f ${heuristic} \
 	-s ${subj} -ss ${session} \
 	-c dcm2niix -b --overwrite 
   fi
 done
 
-# cp sub- to Nifti
-find Nifti_tmp -type d -name 'sub-*' -exec sudo cp -r {} Nifti \;
-rm -rf Nifti_tmp
-
-# change ownership
-sudo chown $USER:$USER Nifti
+# change permission
+find Nifti -type d -exec chmod 755 {} \;
+find Nifti -type f -exec chmod 644 {} \;
 
