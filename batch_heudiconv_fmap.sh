82e1fa9c74c6f144c3cf6cd8ee568f5b853d2a69
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2023-05-26 19:15:20 +0900

    number of slices of filedmap should be specified

--- batch_heudiconv_fmap.sh
+++ batch_heudiconv_fmap.sh
@@ -2,25 +2,27 @@
 
 # script to execute heudiconv for datasets with various fieldmaps
 
-# Usage: batch_heudiconv_fmap.sh heuristic.py subjlist
+# Usage: batch_heudiconv_fmap.sh heuristic.py subjlist slices_of_fieldmap
 # Please see heuristic_template.py and subjlist.sample.txt
 # to prepare the files
 
-# 25 May 2023 K. Nemoto
-
+# 26 May 2023 K. Nemoto
 
 set -x
 
-### number of slices of field mapping image ###
-# Adjust with your dataset
-fmap_slices=39
-fmapthr=$((fmap_slices * 2))
-###
-
+if [[ $# -lt 3 ]]; then
+  echo "Please specify heuristic.py, subjlist, and number_of_slices_of_fieldmap"
+  echo "Usage: $0 heuristic.py subjlist.txt 40"
+  exit 1
+fi
 
 # assign heuristic.py to $heuristic and subjlist.txt to $subjlist
 heuristic=$1
 subjlist=$2
+
+### number of slices of field mapping image ###
+fmap_slices=$3
+fmapthr=$((fmap_slices * 2))
 
 # Make sure you specify a heuristic.py for the first argument
 heuext=${heuristic##*.}
@@ -37,8 +39,8 @@ fi
 # remove blank line beforehand using sed '/^$/d'
 tail +7 ${subjlist} | sed '/^$/d' | while read dname subj session
 do
-  ndirs=$(find DICOM/sorted/${subj}_${session} -type d -name '*field_mapping*' | wc -l)
-  nfiles=$(find DICOM/sorted/${subj}_${session} -type f -name '*field_mapping*' | wc -l)
+  ndirs=$(find DICOM/sorted/${subj}_${session}/*ield* -type d | wc -l)
+  nfiles=$(find DICOM/sorted/${subj}_${session}/*ield* -type f | wc -l)
   if [[ $ndirs -eq 2 ]] && [[ $nfiles -le ${fmapthr} ]]; then
     heudiconv -d DICOM/sorted/${dname}/*/*.dcm \
 	-o Nifti -f ${heuristic} \
