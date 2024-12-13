49da42fb3fb47bcc68c32b0480b0e185f286d638
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2024-12-13 21:37:38 +0900

    update dcm_sort_dir.py

--- dcm_sort_dir.py
+++ dcm_sort_dir.py
@@ -5,9 +5,8 @@
 # Part of this script is based on the script provided by Yuya Saito
 # Prerequisite: pydicom
 
-# 11 Sep 2023 K. Nemoto
+# 15 May 2024 K. Nemoto
 
-import sys
 import os
 import time
 import re
@@ -15,7 +14,7 @@ import shutil
 import argparse
 import pydicom
 
-__version__ = '20230911'
+__version__ = '20240515'
 
 __desc__ = '''
 Sort DICOM files.
@@ -27,55 +26,49 @@ examples:
   dcm_sort_dir.py DICOM_DIR [DICOM_DIR ...]
 '''
 
-def generate_dest_dir_name(dicom_dataset):
+def generate_dest_dir_name(dicom_dataset: pydicom.dataset.FileDataset) -> str:
     series_number = str(dicom_dataset.SeriesNumber).zfill(2)
     series_description = dicom_dataset.SeriesDescription.replace(' ', '_')
     rule_text = f'{series_number}_{series_description}'
-    return re.sub(r'[\\|/|:|?|"|<|>|\|]|\*', '', rule_text)
+    return re.sub(r'[(\\/:?*"<>|)]', '', rule_text)
 
-def copy_dicom_files(src_dir):
-    # Relative path to the sorted directory
-    sorted_dir = '../sorted/'
-
-    # Create the sorted_dir if it doesn't exist
+def copy_dicom_files(src_dir: str, sorted_dir: str = '../sorted/') -> None:
     if not os.path.exists(sorted_dir):
         os.makedirs(sorted_dir)
 
-    # Copy files
-    for root, dirs, files in os.walk(src_dir):
+    for root, _, files in os.walk(src_dir):
         for file in files:
+            src_file = os.path.join(root, file)
             try:
-                src_file = os.path.join(root, file)
                 ds = pydicom.dcmread(src_file)
                 if hasattr(ds, 'pixel_array'):
                     dest_dir_name = generate_dest_dir_name(ds)
-                    out_dir = f'{sorted_dir}{src_dir.replace("/", "")}'
-                    print(src_file, dest_dir_name)
+                    out_dir = os.path.join(sorted_dir, os.path.basename(src_dir))
                     dest_dir = os.path.join(out_dir, dest_dir_name)
                     os.makedirs(dest_dir, exist_ok=True)
                     shutil.copy2(src_file, dest_dir)
-                    print(f"copy {src_file} -> {dest_dir}")
-            except:
-                pass
+                    print(f"Copy {src_file} -> {dest_dir}")
+            except Exception as e:
+                print(f"Failed to process {src_file}: {e}")
 
-def main():
+def main() -> int:
     start_time = time.time()
     parser = argparse.ArgumentParser(description=__desc__, epilog=__epilog__,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('dirs', metavar='DICOM_DIR', help='DICOM directory or directories to process.', nargs='+')
 
-    err = 0
     try:
         args = parser.parse_args()
         for dir in args.dirs:
-            print(dir)
+            print(f"Processing directory: {dir}")
             copy_dicom_files(dir)
-        print(f"Execution time: {time.time() - start_time:.2f} seconds.")
+        elapsed_time = time.time() - start_time
+        print(f"Execution time: {elapsed_time:.2f} seconds.")
+        return 0
     except Exception as e:
-        print(f"{__file__}: error: {str(e)}")
-        err = 1
-
-    sys.exit(err)
+        print(f"Error: {e}")
+        return 1
 
 if __name__ == '__main__':
-    main()
+    import sys
+    sys.exit(main())
