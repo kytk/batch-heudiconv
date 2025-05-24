6ceaefbf344e37936985e0e96c7c6506520ceee1
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2025-05-24 15:38:51 +0900

    use <study_name> from now on

--- bh01_prep_dir.sh
+++ bh01_prep_dir.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 # preparation script for DICOM to BIDS conversion using heudiconv
 # Part1. organize directory structure
-# K.Nemoto 13 Dec 2024
+# K.Nemoto 24 May 2025
 
 # For debugging
 # set -x
 
 if [[ $# -lt 1 ]]; then
-    echo "Please specify a name of sequence sets"
-    echo "Usage: $0 <name of sequence set>"
+    echo "Create a new BIDS conversion study with organized directory structure"
+    echo "Usage: $0 <study_name>"
+    echo ""
+    echo "Examples:"
+    echo "  $0 resting_state_2024    # Creates 'resting_state_2024' study"
+    echo "  $0 pilot_dwi_study       # Creates 'pilot_dwi_study' study"
+    echo "  $0 longitudinal_cohort   # Creates 'longitudinal_cohort' study"
+    echo ""
+    echo "This will create a complete workspace under '<study_name>/' containing:"
+    echo "  - DICOM/original/   : Place your DICOM files here"
+    echo "  - DICOM/sorted/     : For organized DICOM files"
+    echo "  - bids/rawdata/     : Final BIDS output"
+    echo "  - bids/derivatives/ : For processed data"
+    echo "  - code/             : Heuristic files"
+    echo "  - tmp/              : Working files"
     exit 1
 fi
 
-# First argument is a name of sequence set (e.g. MR0042)
-setname=${1%/}
+# First argument is a name of study (e.g. resting_state_2024)
+study_name=${1%/}
 
 # Specify the path of bh00_addpath.sh
 batchpath=$(dirname $(command -v bh00_addpath.sh))
 
 # prepare working directory
-[[ -d $setname ]] || mkdir $setname
-cd $setname
+[[ -d $study_name ]] || mkdir $study_name
+cd $study_name
 
 # prepare directory structure
 declare -a dirs=(
@@ -41,17 +54,25 @@ done
 if [[ -d ${batchpath}/code ]]; then
     cp -r ${batchpath}/code/* code/
 else
-    echo "Warning: No heuristics found in ${batchpath}/code"
+    echo "Warning: No template heuristics found in ${batchpath}/code"
 fi
 
-echo "Directory structure for ${setname} has been prepared:"
-echo "- DICOM/original/  : Place your original DICOM files here"
-echo "- DICOM/sorted/    : Sorted DICOM files will be stored here"
-echo "- DICOM/converted/ : Backup of processed DICOM files"
-echo "- bids/           : BIDS-formatted output will be stored here"
-echo "- code/           : Location for heuristic files"
-echo "- tmp/            : Temporary files"
-echo
-echo "Please copy DICOM files to DICOM/original/ directory to proceed."
+echo "Directory structure for study '${study_name}' has been prepared:"
+echo ""
+echo "Next steps:"
+echo "1. Copy your DICOM files to: ${study_name}/DICOM/original/"
+echo "2. Run: bh02_sort_dicom.sh ${study_name}"
+echo ""
+echo "Directory structure created:"
+echo "  ${study_name}/"
+echo "  ├── DICOM/"
+echo "  │   ├── original/    # Place your original DICOM files here"
+echo "  │   ├── sorted/      # Sorted DICOM files will be stored here"
+echo "  │   └── converted/   # Backup of processed DICOM files"
+echo "  ├── bids/"
+echo "  │   ├── rawdata/     # BIDS-formatted output will be stored here"
+echo "  │   └── derivatives/ # Processed data location"
+echo "  ├── code/            # Heuristic files for this study"
+echo "  └── tmp/             # Working files"
 
 exit 0
